66d98ab5dc2fa69f11993b6de06e9841f1a9534a
Alexandra Drinkwater <alexandradawndrinkwater@gmail.com>
2016-05-18 16:20:50 +1000

    PSVR Website
    New PSVR website

---
+++ psvrwebsite/mail.php
@@ -0,0 +1,15 @@
+<?php
+	include_once('config.php');
+	if(isset($_POST['submit']))
+{
+	
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "info@saferemovals.net.au";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
